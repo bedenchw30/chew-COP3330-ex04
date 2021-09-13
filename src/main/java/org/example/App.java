f6652fc1cfffc6f3dec49b47e77f414f22bfffca
bedenchw30 <90423350+bedenchw30@users.noreply.github.com>
2021-09-13 17:17:57 -0400

    initial commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 import java.util.Scanner;
+/**
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 beden chew
+ */
 public class App
 {
     public static void main( String[] args )
